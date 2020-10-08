6e38f0aca46e96d0a759d8789fde897b4b7562d8
Madan <58905767+Madanaa@users.noreply.github.com>
2020-10-08 03:38:06 -0700

    Solution.java:14: error: cannot find symbol
    Solution.java:14: error: cannot find symbol 
    
    myClass change MyClass

--- java-regex/Solution.java
+++ java-regex/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
 	}
 }
 
-class myRegex {
+class MyRegex {
 	private String part = "((25[0-5])|(2[0-4][0-9])|([0-1]{0,1}[0-9]{1,2}))";
 	String pattern = String.format("%s\\.%s\\.%s\\.%s", part, part, part, part);
 }
